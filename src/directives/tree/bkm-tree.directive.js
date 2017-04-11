5d6c4dc02ec3f14f47e34f79bf927b7e2023bdd0
zcd0000 <155466@qq.com>
2017-04-11 21:38:33 +0800

    优化bkm-tree的模板代码和样式

--- src/directives/tree/bkm-tree.directive.js
+++ src/directives/tree/bkm-tree.directive.js
@@ -37,6 +37,22 @@
             bkmInputTreeAddressCtrl
         ]);
 
+    var treeTemplate =
+          '<div class="tree-address">\
+                <div temp></div>\
+                <span data-bkm-address class="icon glyphicon glyphicon-list" ng-click="dCtrl.showMenu($event)"></span>\
+                <div class="ztree-box" ng-show="dCtrl.show"> \
+                   <div class="ztree-box-container">\
+                     <div class="ztree-btn-clean">\
+                        <a data-bkm-address href="javascript:void(0);" ng-click="dCtrl.clearInput($event);">清空输入</a>\
+                        &nbsp;&nbsp;&nbsp;&nbsp;\
+                        <a data-bkm-address href="javascript:void(0);" ng-click="$event.stopPropagation(); dCtrl.show=false">关闭</a>\
+                      </div>\
+                      <ul bkm-tree="dCtrl.setting" class="ztree"></ul>\
+                   </div>\
+                </div>\
+            </div>';
+
     function bkmZtreeData($window, regionAipService, $q) {
         var self = this;
         self.getData = function (parentId) {
@@ -76,12 +92,13 @@
             scope: {
                 setting: '=bkmTree'
             },
+            controller: function () {
+            },
             link: function ($scope, iElm, iAttrs, controller) {
                 $scope.setting.treeInstance = null;
-
                 //清除选中节点的选中状态
                 $scope.setting.cleanSelectedNodes = function () {
-                    if (!!!$scope.setting.treeInstance)return;
+                    if (!!!$scope.setting.treeInstance) return;
                     var nodes = $scope.setting.treeInstance.getSelectedNodes();
                     angular.forEach(nodes, function (v, i) {
                         $scope.setting.treeInstance.cancelSelectedNode(v);
@@ -90,7 +107,7 @@
 
                 //当子节点被选中时，将所有父节点设置为选中状态
                 $scope.setting.selectParenNode = function (treeNode) {
-                    if (!!!$scope.setting.treeInstance)return;
+                    if (!!!$scope.setting.treeInstance) return;
                     $scope.setting.treeInstance.selectNode(treeNode, true);
                     var p = treeNode.getParentNode();
                     if (p) {
@@ -100,7 +117,7 @@
 
                 //展开节点下的所有了节点
                 function expandNode(node) {
-                    if (!!!$scope.setting.treeInstance || !!!node)return;
+                    if (!!!$scope.setting.treeInstance || !!!node) return;
                     $scope.setting.treeInstance.expandNode(node, true);
                     var c = node.children;
                     angular.forEach(c, function (v, i) {
@@ -125,6 +142,9 @@
                 if ($scope.setting.nodes && $scope.setting.nodes.length) {
                     $scope.setting.treeInstance = $.fn.zTree.init(iElm, $scope.setting, $scope.setting.nodes);
                 }
+
+                //节点数据初始化回调
+                $scope.setting.setNodesDataSource();
             }
         };
     }
@@ -153,26 +173,12 @@
                 //【节点】点击事件
                 onClick: onClick,
                 beforeExpand: beforeExpand
-            }
+            },
+            setNodesDataSource: setNodesDataSource
         };
 
         function beforeExpand(treeId, treeNode) {
-            if (!!!treeNode.children || !!!treeNode.children.length) {
-                treeDataService
-                    .getData(treeNode.id)
-                    .then(function (data) {
-                        var newNode = convertData(data, treeNode.level + 1 < ctrl.opt.chooseLevel);
-                        ctrl.setting.treeInstance.addNodes(treeNode, newNode);
-                    })
-                    .catch(function () {
-                        console.log('get tree data error');
-                    });
-
-            } else {
-                ctrl.setting.treeInstance.expandNode(treeNode, true, false, null);
-            }
-            return false;
-
+            return beforeExpandFn(treeDataService, treeNode, ctrl.setting.treeInstance, ctrl.opt.chooseLevel);
         }
 
         //【节点】点击事件
@@ -191,7 +197,7 @@
                 ctrl.show = false;
             }, 0);
         }
-
+        //将上次展开的节点收起来
         ctrl.showMenu = function (e) {
             e.stopPropagation();
             ctrl.setting.cleanSelectedNodes();
@@ -230,16 +236,19 @@
             }
         }
 
-        //初始化省市区数据
-        treeDataService
-            .getData('')
-            .then(function (data) {
-                var newNode = convertData(data, 0 < ctrl.opt.chooseLevel);
-                ctrl.setting.setNodes(newNode);
-            })
-            .catch(function () {
-                console.log('get tree data error');
-            });
+        //初始化数据回调函数
+        function setNodesDataSource() {
+            //初始化省市区数据
+            treeDataService
+                .getData('')
+                .then(function (data) {
+                    var newNode = convertData(data, 0 < ctrl.opt.chooseLevel);
+                    ctrl.setting.setNodes(newNode);
+                })
+                .catch(function () {
+                    console.log('get tree data error');
+                });
+        }
 
         function convertData(data, isParent) {
             angular.forEach(data, function (v, i) {
@@ -252,23 +261,29 @@
             });
             return data;
         }
+
+        function beforeExpandFn(treeDataSvc, treeNode, treeInstance, chooseLevel) {
+            if (!!!treeNode.children || !!!treeNode.children.length) {
+                treeDataSvc.getData(treeNode.id)
+                    .then(function (data) {
+                        var newNode = convertData(data, treeNode.level + 1 < chooseLevel);
+                        treeInstance.addNodes(treeNode, newNode);
+                    })
+                    .catch(function () {
+                        console.log('get tree data error');
+                    });
+
+            } else {
+                treeInstance.expandNode(treeNode, true, false, null);
+            }
+            return false;
+        }
+
+        
     }
 
     function inputTree($compile, $timeout) {
-        var template = '<div class="tree-address">' +
-            '<div temp></div>' +
-            '<span data-bkm-address class="icon glyphicon glyphicon-list" ng-click="dCtrl.showMenu($event)"></span>' +
-            '<div class="ztree-box" ng-show="dCtrl.show"> ' +
-            '<div class="ztree-box-container">' +
-            '<div class="ztree-btn-clean">' +
-            '<a data-bkm-address href="javascript:void(0);" ng-click="dCtrl.clearInput($event);">清空输入</a>' +
-            '&nbsp;&nbsp;&nbsp;&nbsp;' +
-            '<a data-bkm-address href="javascript:void(0);" ng-click="$event.stopPropagation(); dCtrl.show=false">关闭</a>' +
-            '</div>' +
-            '<ul bkm-tree="dCtrl.setting" class="ztree"></ul>' +
-            '</div>' +
-            '</div>' +
-            '</div>';
+        
         return {
             restrict: 'EA',
             require: "ngModel",
@@ -277,10 +292,10 @@
             scope: {},
             link: function (scope, elem, attr, ngModel) {
                 elem.attr("readonly", true);
-                angular.extend(scope.dCtrl.opt, {chooseLevel: attr.chooseLevel, showFullName: attr.showFullName});
+                angular.extend(scope.dCtrl.opt, { chooseLevel: attr.chooseLevel, showFullName: attr.showFullName });
                 scope.dCtrl.ngModel = ngModel;
                 scope.dCtrl.showFullName = attr.showFullName.toLowerCase() === 'true';
-                var el = angular.element(template);
+                var el = angular.element(treeTemplate);
                 //如果不为 ztree 设置id,则同一个页面有多个 atree 时，将会是同一个实例
                 el.find('.ztree').attr("id", newGuid());
                 var temp = $compile(el)(scope);
@@ -315,6 +330,7 @@
                     return value;
                 });
 
+                //点击除树窗口区域外的地方，响应click事件关闭树窗口
                 angular.element(document).on('click', function (e) {
                     if (e.target.hasAttribute("data-bkm-address")) {
                         return;
@@ -324,25 +340,39 @@
                         }, 0);
                     }
                 });
-
+                //在当前树窗口区域内点击时，将事件阻止掉，避免向父元素传递click事件
                 el.find("ul.ztree").on("click", function (e) {
                     e.stopPropagation();
                 });
 
-                function newGuid() {
-                    var guid = "";
-                    for (var i = 1; i <= 32; i++) {
-                        var n = Math.floor(Math.random() * 16.0).toString(16);
-                        guid += n;
-                        if ((i == 8) || (i == 12) || (i == 16) || (i == 20))
-                            guid += "-";
-                    }
-                    return guid;
-                }
+                
             }
         };
     }
+
+
+    function newGuid() {
+        var guid = "";
+        for (var i = 1; i <= 32; i++) {
+            var n = Math.floor(Math.random() * 16.0).toString(16);
+            guid += n;
+            if ((i == 8) || (i == 12) || (i == 16) || (i == 20))
+                guid += "-";
+        }
+        return guid;
+    }
+
+    
+
+   
+
+    
+
+
+
 })();
+
+
 
 /*
 
