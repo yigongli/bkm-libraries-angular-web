8f844dc09f396a60315a9d580769b936bcd83139
gurihui <rihui.gu@foxmail.com>
2021-04-18 13:06:14 +0800

    onParseSuccess 回调中返回 input 对象

--- src/directives/search/bkmImportSheet.directive.js
+++ src/directives/search/bkmImportSheet.directive.js
@@ -24,7 +24,7 @@
                 fileType = angular.isArray(opts.fileType) ? opts.fileType : ['xlsx', 'xls'];
             // 初始化返回的文件对象
             opts.file = {};
-        
+
             // 监听文件变化
             $elem.on('change', onInputChange);
 
@@ -35,7 +35,7 @@
                     toastr.info(bkm.util.format('请选择文件后缀为{0}类型的文件!', fileType.join(',')));
                     return;
                 }
-                
+
                 // 设置返回的文件对象
                 angular.extend(opts.file, {
                     name: file.name,
@@ -68,11 +68,11 @@
                     // 重置监测状态
                     resetInputFile(targetElem);
                 }
-                
+
             }
 
             // 解析Excel文件
-            function parseSheet(e, onParseSuccess) {
+            function parseSheet(f, onParseSuccess) {
                 var reader = new FileReader();
                 reader.onload = function (e) {
                     /* read workbook */
@@ -94,11 +94,11 @@
                             });
                         });
                         if (angular.isFunction(onParseSuccess)) {
-                            onParseSuccess(sheets);
+                            onParseSuccess(sheets, f);
                         }
                     }
                 };
-                reader.readAsBinaryString(e.target.files[0]);
+                reader.readAsBinaryString(f.target.files[0]);
             }
 
             function resetInputFile(elem) {
